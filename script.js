47de8846b9c97c9b372eb32d7c637f694dc1e45f
AFPL <arthurferndo.luz07@hotmail.com>
2025-01-15 20:09:12 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('protection-button').addEventListener('click', function () {
+    alert('You are now protected against Loiras, Franjudas, and Cacheadas who alisam o cabelo!');
+});
