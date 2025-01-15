67e44af4d1162fce5d51e28881b637b69cc74e83
AFPL <arthurferndo.luz07@hotmail.com>
2025-01-15 20:23:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.getElementById('vpn-button').addEventListener('click', function () {
-    alert('VPN ativada! Você agora está protegido contra golpes online e navega com segurança.');
+    alert('VPN ativada! Você agora está protegido contra loiras, franjudas e cacheadas que alisam o cabelo!');
 });
+
+// Função para exibir alerta de ataque após 40 segundos
+setTimeout(function () {
+    document.getElementById('attack-warning').style.display = 'block';
+    setTimeout(function () {
+        alert('Alerta! Sua ex-namorada está tentando te atacar novamente!');
+    }, 2000);
+}, 40000); // 40 segundos
